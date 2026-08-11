8140ae66efdd65c0ef2409157d3d3134aa8ead10
mdsuhail270907-crypto <mdsuhail270907@users.noreply.github.com>
2026-08-11 14:20:26 +0530

    Add auth backend setup

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
@@ -10,14 +11,24 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+// Middleware
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
+
 app.use(express.json());
 
+// Health check
 app.get("/api/health", (req, res) => {
   res.json({
-    message: "SyncDoc Backend is Running"
+    message: "SyncDoc Backend is Running",
   });
 });
+
+// Authentication routes
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
